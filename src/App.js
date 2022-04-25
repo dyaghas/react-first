04eace7595cb21a73236a3d93e0e25f660fec7e7
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-04-25 17:56:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+const displayEmojiName = event => alert(event.target.id);
+
 function App() {
+    const greeting = "greeting";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 id="greeting">Hello World!</h1>
+      <p>Writing a JSX</p>
+      <ul>
+        <li>
+          <button 
+            onClick={displayEmojiName}
+          >
+            <span role="img" aria-label="grinning face" id="grinning face">😀</span>
+          </button>
+        </li>
+        <li>
+          <button 
+            onClick={displayEmojiName}
+          >
+            <span role="img" aria-label="party popper" id="party popper">🎉</span>
+          </button>
+        </li>
+        <li>
+          <button 
+            onClick={displayEmojiName}
+          >
+            <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }

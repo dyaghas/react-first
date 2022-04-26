dc27a2cb593487d5637b648927352168bfea0832
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-04-26 11:07:15 -0300

    idk

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import './App.css';
 
 const displayEmojiName = event => alert(event.target.id);
 
+//thats an array
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face"
+  },
+  {
+    emoji: "🎉",
+    name: "party popper"
+  },
+  {
+    emoji: "💃",
+    name: "woman dancing"
+  }
+]
+
 function App() {
     const greeting = "greeting";
   return (
@@ -9,27 +25,17 @@ function App() {
       <h1 id="greeting">Hello World!</h1>
       <p>Writing a JSX</p>
       <ul>
-        <li>
-          <button 
-            onClick={displayEmojiName}
-          >
-            <span role="img" aria-label="grinning face" id="grinning face">😀</span>
-          </button>
-        </li>
-        <li>
-          <button 
-            onClick={displayEmojiName}
-          >
-            <span role="img" aria-label="party popper" id="party popper">🎉</span>
-          </button>
-        </li>
-        <li>
-          <button 
-            onClick={displayEmojiName}
-          >
-            <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
-          </button>
-        </li>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
